5e2d9bd94e9fda59592ff4dafd46d4d25ce3027a
Naresh <naresh.k@teleparadigm.com>
2014-12-12 10:39:47 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DCSH::Application.config.secret_token = 'ae4005a58f144dfecb5e344d753cd800a6750e0f0907f0ad5730c778f90d3d47a1ac898ef97412135d51def97bb61a5c1bba754c9dd8b41a723b4a8642f9d3a0'
